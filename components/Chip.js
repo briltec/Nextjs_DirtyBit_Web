47f3075db9880570a5650bc26a37d4283d98b310
briltec <palmerliu0625@gmail.com>
2021-10-19 16:01:16 +0530

    Chip array added

---
+++ components/Chip.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Chip from '@mui/material/Chip';
+import Paper from '@mui/material/Paper';
+import TagFacesIcon from '@mui/icons-material/TagFaces';
+
+const ListItem = styled('li')(({ theme }) => ({
+  margin: theme.spacing(0.5),
+}));
+
+export default function ChipsArray() {
+  const [chipData, setChipData] = React.useState([
+    { key: 0, label: 'Angular' },
+    { key: 1, label: 'jQuery' },
+    { key: 2, label: 'Polymer' },
+    { key: 3, label: 'React' },
+    { key: 4, label: 'Vue.js' },
+  ]);
+
+  const handleDelete = (chipToDelete) => () => {
+    setChipData((chips) => chips.filter((chip) => chip.key !== chipToDelete.key));
+};
+
+    console.log(chipData)
+  return (
+    <Paper
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+        listStyle: 'none',
+        p: 0.5,
+        m: 0,
+      }}
+      component="ul"
+    >
+      {chipData.map((data) => {
+        let icon;
+
+        return (
+          <ListItem key={data.key}>
+            <Chip
+            color="warning"
+              icon={icon}
+              label={data.label}
+              onDelete={handleDelete(data)}
+              onClick={() => console.log('You clicked the Chip.')}
+            />
+          </ListItem>
+        );
+      })}
+    </Paper>
+  );
+}
