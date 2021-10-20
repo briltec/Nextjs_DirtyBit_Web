a8069c182243a91b26e4e33d1b4f0b95fbbe7996
briltec <palmerliu0625@gmail.com>
2021-10-20 14:48:15 +0530

    conditional navbar background

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,25 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { store } from "../redux/store";
 import "../styles/index.css";
+import {useRouter} from 'next/router'
 
 function MyApp({ Component, pageProps }) {
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />);
   }
+  const router = useRouter()
+  
+  let background;
+  let fixedTop = '';
+  if(router.pathname === '/'){
+    background = 'bg-transparent-800 w-screen absolute'
+  }else {
+    background = 'bg-black border-b-1 border-gray-500 w-screen'
+    fixedTop = 'sticky top-0 z-50'
+  }
   return (
     <>
-      <Navbar />
+      <Navbar fixedHeader={fixedTop} bg={background} />
       <Component {...pageProps} />
       <Footer />
     </>
