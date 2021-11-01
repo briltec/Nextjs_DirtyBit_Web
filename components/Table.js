311021e31b77cf8e2a3215252fcf9fbbf87525b2
briltec <palmerliu0625@gmail.com>
2021-11-01 14:15:41 +0530

    populated with data

--- components/Table.js
+++ components/Table.js
@@ -1,39 +1,49 @@
 import React from 'react'
 
-function Table() {
+function Table({list}) {
+    let problemLevel;
+    let problemColor;
+    let problemMarkup = (
+      list.map((problem, idx) => {
+        switch(problem.problem_level){
+            case 'E':
+              problemLevel = 'Easy'
+              problemColor = 'text-green-500'
+              break;
+            case 'M':
+              problemLevel = 'Medium'
+              problemColor = 'text-yellow-500'
+              break;
+            case 'H':
+              problemLevel = 'Hard'
+              problemColor = 'text-red-500'
+              break;
+        }
+        return (
+          <tr key={problem.id} className="hover:bg-grey-lighter">
+          <td className="table-data">{idx+1}.{problem.title}</td>
+          <td className="py-4 px-6 border-b border-grey-light">
+            <a href="#" className={` ${problemColor} font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark`}>{problemLevel}</a>
+          </td>
+          <td className="table-data">➖</td>
+        </tr>
+        )
+      })
+    )
+    
     return (
-        <div class="lg:w-2/3 w-1/5">
-  <div class="bg-[#060F1E] shadow-md rounded my-6">
-    <table class="text-left w-full border-collapse">
+        <div className="lg:w-2/3 md:w-full sm:w-full xs:w-full">
+  <div className="bg-[#060F1E] shadow-md rounded my-6">
+    <table className="text-left w-full border-collapse">
       <thead>
         <tr>
-          <th class="py-4 px-6 bg-grey-lightest font-bold text-base text-[#686868] border-b border-grey-light">Problem title</th>
-          <th class="py-4 px-6 bg-grey-lightest font-bold text-base text-[#686868] text-grey-dark border-b border-grey-light">Difficulty</th>
-          <th class="py-4 px-6 bg-grey-lightest font-bold text-base text-[#686868] text-grey-dark border-b border-grey-light">Status</th>
+          <th className="table-heading">Problem title</th>
+          <th className="table-heading">Difficulty</th>
+          <th className="table-heading">Status</th>
         </tr>
       </thead>
       <tbody>
-        <tr class="hover:bg-grey-lighter">
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">1. Two Sum</td>
-          <td class="py-4 px-6 border-b border-grey-light">
-            <a href="#" class="text-green-500 font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Easy</a>
-          </td>
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">➖</td>
-        </tr>
-        <tr class="hover:bg-grey-lighter">
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">2. Add Two Numbers</td>
-          <td class="py-4 px-6 border-b border-grey-light">
-            <a href="#" class="text-yellow-400 font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Medium</a>
-          </td>
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">✔️</td>
-        </tr>
-        <tr class="hover:bg-grey-lighter">
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">3. Median of Two Sorted Arrays</td>
-          <td class="py-4 px-6 border-b border-grey-light">
-            <a href="#" class="text-red-600 font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Hard</a>
-          </td>
-          <td class="py-4 px-6 border-b border-grey-light font-medium hover:text-blue-700 transition-all ease-in-out duration-100 cursor-pointer">✔️</td>
-        </tr>
+        {problemMarkup}     
       </tbody>
     </table>
   </div>
