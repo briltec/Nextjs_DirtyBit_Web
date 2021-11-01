6a07ed0b1205fb3cc669d8e69c17dca299651d73
briltec <palmerliu0625@gmail.com>
2021-11-01 15:07:01 +0530

    updated

--- pages/practice.js
+++ pages/practice.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import Head from "next/head";
-import Cookies from "js-cookie";
 import Gettoken from "../components/Helper/Gettoken";
 
 import Problem from "../components/Problem";
@@ -20,17 +19,13 @@ function practice({ problemList }) {
 export const getServerSideProps = async (ctx) => {
   const { req, res } = ctx;
   let response;
-  console.log("Ok");
-  let refresh_token = Cookies.get("refresh");
-  console.log(refresh_token);
+  let refresh_token = req.cookies.refresh;
   if (refresh_token) {
-    console.log("Here");
-    await Gettoken(Cookies.get("refresh"));
+    await Gettoken(req.cookies.refresh);
     const headers = {
       "Content-Type": "application/json",
-      Authorization: "JWT " + Cookies.get("access"),
+      Authorization: "JWT " + req.cookies.access,
     };
-    console.log(headers);
     response = await getProblemsList.post(
       "/",
       { data: {} },
@@ -39,10 +34,8 @@ export const getServerSideProps = async (ctx) => {
       }
     );
   } else {
-    console.log("There");
     response = await getProblemsList.post("/");
   }
-  // console.log(response.data);
   return {
     props: {
       problemList: response.data,
