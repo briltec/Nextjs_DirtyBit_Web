bcc84b77cce6c1b3b54526cca834046b5767c539
briltec <palmerliu0625@gmail.com>
2021-11-11 17:34:25 +0530

    localhost source for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['encrypted-tbn0.gstatic.com','lh3.googleusercontent.com'],
+    domains: ['encrypted-tbn0.gstatic.com','lh3.googleusercontent.com', 'miro.medium.com', 'localhost'],
   },
 }
