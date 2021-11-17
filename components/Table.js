19bd638f92088e60e30bbd4d228e1851a4ed3d66
briltec <palmerliu0625@gmail.com>
2021-11-17 22:49:04 +0530

    updated

--- components/Table.js
+++ components/Table.js
@@ -1,68 +1,74 @@
-import TableLoader from './TableLoader';
+import TableLoader from "./TableLoader";
 
-function Table({list}) {
-    let problemLevel;
-    let problemColor;
-    let status;
+function Table({ list }) {
+  let problemLevel;
+  let problemColor;
+  let status;
 
-    console.log('list', list)
-    let problemMarkup = (
-      list.map((problem, idx) => {
-        switch(problem.problem_level){
-            case 'E':
-              problemLevel = 'Easy'
-              problemColor = 'text-green-500'
-              break;
-            case 'M':
-              problemLevel = 'Medium'
-              problemColor = 'text-yellow-500'
-              break;
-            case 'H':
-              problemLevel = 'Hard'
-              problemColor = 'text-red-500'
-              break;
-        }
-        switch(problem.solved){
-          case 'Unsolved':
-              status = '➖'
-              break;
-          case 'Solved':
-              status = '✔️'
-              break;
-          case 'Attempted':
-              status = '❓'
-              break;
-        } 
-        return (
-          <tr key={problem.id} className="hover:bg-grey-lighter">
-          <td className="table-data">{idx+1} . {problem.title}</td>
-          <td className="py-4 px-6 border-b border-grey-light">
-            <a href="#" className={` ${problemColor} font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark`}>{problemLevel}</a>
-          </td>
-          <td className="table-data">{status}</td>
-        </tr>
-        )
-      })
-    )
-    
+  console.log("list", list);
+  let problemMarkup = list.map((problem, idx) => {
+    switch (problem.problem_level) {
+      case "E":
+        problemLevel = "Easy";
+        problemColor = "text-green-500";
+        break;
+      case "M":
+        problemLevel = "Medium";
+        problemColor = "text-yellow-500";
+        break;
+      case "H":
+        problemLevel = "Hard";
+        problemColor = "text-red-500";
+        break;
+    }
+    switch (problem.solved) {
+      case "Unsolved":
+        status = "➖";
+        break;
+      case "Solved":
+        status = "✔️";
+        break;
+      case "Attempted":
+        status = "❓";
+        break;
+    }
     return (
-        <div className="lg:w-2/3 md:w-full sm:w-full xs:w-full">
-  <div className="bg-[#060F1E] shadow-md rounded my-6">
-    <table className="text-left w-full border-collapse">
-      <thead>
-        <tr>
-          <th className="table-heading">Problem title</th>
-          <th className="table-heading">Difficulty</th>
-          <th className="table-heading">Status</th>
-        </tr>
-      </thead>
-      <tbody>
-        {list.length > 0 ? problemMarkup : <TableLoader />}
-      </tbody>
-    </table>
-  </div>
-</div>
-    )
+      <tr key={problem.id} className="hover:bg-grey-lighter">
+        <td className="table-data">
+          {idx + 1} . {problem.title}
+        </td>
+        <td className="py-4 px-6 border-b border-grey-light">
+          <a
+            href="#"
+            className={` ${problemColor} font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark`}
+          >
+            {problemLevel}
+          </a>
+        </td>
+        <td className="table-data">{status}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <div className="lg:w-2/3 md:w-full sm:w-full xs:w-full">
+      <div className="bg-[#060F1E] shadow-md rounded my-6">
+        <table className="text-left w-full border-collapse">
+          <thead>
+            <tr>
+              <th className="table-heading">Problem title</th>
+              <th className="table-heading">Difficulty</th>
+              <th className="table-heading">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* {list.length > 0 ? problemMarkup : <TableLoader />} */}
+            <TableLoader />
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
--- components/TableLoader.js
+++ components/TableLoader.js
@@ -1,8 +1,8 @@
-import React from "react"
-import ContentLoader from "react-content-loader"
+import React from "react";
+import ContentLoader from "react-content-loader";
 
 const MyLoader = (props) => (
-  <ContentLoader 
+  <ContentLoader
     speed={2}
     width={800}
     height={400}
@@ -11,17 +11,16 @@ const MyLoader = (props) => (
     foregroundColor="#8566db"
     {...props}
   >
-    <rect x="9" y="29" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="9" y="61" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="8" y="97" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="9" y="132" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="6" y="264" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="9" y="166" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="7" y="232" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="8" y="199" rx="7" ry="7" width="381" height="24" /> 
-    <rect x="7" y="295" rx="7" ry="7" width="381" height="24" />
+    <rect x="9" y="29" rx="7" ry="7" width="1500" height="24" />
+    <rect x="9" y="61" rx="7" ry="7" width="1500" height="24" />
+    <rect x="8" y="97" rx="7" ry="7" width="1500" height="24" />
+    <rect x="9" y="132" rx="7" ry="7" width="1500" height="24" />
+    <rect x="6" y="264" rx="7" ry="7" width="1500" height="24" />
+    <rect x="9" y="166" rx="7" ry="7" width="1500" height="24" />
+    <rect x="7" y="232" rx="7" ry="7" width="1500" height="24" />
+    <rect x="8" y="199" rx="7" ry="7" width="1500" height="24" />
+    <rect x="7" y="295" rx="7" ry="7" width="1500" height="24" />
   </ContentLoader>
-)
+);
 
-export default MyLoader
-
+export default MyLoader;
