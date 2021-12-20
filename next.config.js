4ac774e823a634abbd4ce12e4761063c6a34c739
briltec <palmerliu0625@gmail.com>
2021-12-20 21:36:21 +0530

    next js config for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['encrypted-tbn0.gstatic.com','lh3.googleusercontent.com', 'miro.medium.com', 'localhost'],
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "lh3.googleusercontent.com",
+      "miro.medium.com",
+      "localhost",
+      "img.icons8.com",
+      "helostatus.com",
+    ],
   },
-}
+};
