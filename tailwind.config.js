3b08a0af42520be7677eab9066f4c3c4d1cfb164
briltec <palmerliu0625@gmail.com>
2021-12-22 21:28:02 +0530

    daisy ui plugin added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,27 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    mode: 'jit',
-    purge: ['./pages/**/*.js', './styles/**/*.css', './components/**/*.js'],
-    future: {
-      removeDeprecatedGapUtilities: true,
-      purgeLayersByDefault: true,
+  mode: "jit",
+  purge: ["./pages/**/*.js", "./styles/**/*.css", "./components/**/*.js"],
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  darkMode: "class",
+  plugins: [require("tailwind-scrollbar-hide"), require("daisyui")],
+  theme: {
+    screens: {
+      xs: "630px",
+      ...defaultTheme.screens,
     },
-    darkMode: 'class',
-    plugins: [
-      require('tailwind-scrollbar-hide')
-    ],
-    theme: {
-      screens: {
-        'xs': '630px',
-        ...defaultTheme.screens,
+    extend: {
+      colors: {
+        "custom-yellow": "#FB7A16",
+        "custom-yellow2": "#FF6663",
+        "custom-maroon": "#410B3B",
+        "custom-background": "#06202A",
+        "custom-bg": "#AB61F7",
       },
-      extend: {
-        colors: {
-          'custom-yellow':'#FB7A16',
-          'custom-yellow2': '#FF6663',
-          'custom-maroon' : '#410B3B',
-          'custom-background': '#06202A',
-          'custom-bg': '#AB61F7'
-        }
-      },
-    },  
-  };
+    },
+  },
+};
