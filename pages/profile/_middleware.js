9b590d0869ae66d8dc1621d724af60c89f807516
briltec <palmerliu0625@gmail.com>
2022-01-12 22:53:01 +0530

    middleware for profile page to check authenticated user

--- pages/profile/_middleware.js
+++ pages/profile/_middleware.js
@@ -1,8 +1,9 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req, ev) {
+  console.log("req", req.nextUrl);
   if (!req.cookies["access"]) {
     return NextResponse.redirect("/auth/signin");
   }
-  return NextResponse.next();
+  return NextResponse.rewrite(req.nextUrl);
 }
