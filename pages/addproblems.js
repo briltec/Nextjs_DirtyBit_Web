0ed78224197f1957ddb4227a76ee44655e867605
briltec <palmerliu0625@gmail.com>
2022-01-28 20:24:06 +0530

    code refactored

--- pages/addproblems.js
+++ pages/addproblems.js
@@ -6,11 +6,12 @@ import { Page1 } from "../components/AddProblemComponents/Page1";
 import { Page2 } from "../components/AddProblemComponents/Page2";
 import { getTags } from "../redux/actions/index";
 import { useDispatch } from "react-redux";
+import New from "../components/AddProblemComponents/New";
 
 function addproblems(props) {
   const dispatch = useDispatch();
   const tags = useSelector((state) => state.tags);
-  let [step, setStep] = useState(1);
+  // let [step, setStep] = useState(1);
   let [probId, setProbId] = useState(null);
 
   useEffect(() => {
@@ -21,33 +22,14 @@ function addproblems(props) {
 
   return (
     <div>
-      {step === 1 ? (
+      {/* {step === 1 ? (
         <Page1 stepFunction={setStep} probIdFunction={setProbId} tags={tags} />
       ) : (
         <Page2 stepFunction={setStep} problemId={probId} />
-      )}
+      )} */}
+      <New tagsValue={tags} />
     </div>
   );
 }
-
-// export const getServerSideProps = async (ctx) => {
-//   const response = await axios.get(
-//     "https://db-code.herokuapp.com/problems/getTagListCreateProblem/"
-//   );
-//   const parseData = response.data.results;
-//   let colourOptions = [];
-//   for (const data in parseData) {
-//     let tempData = {};
-//     tempData["value"] = parseData[data].id;
-//     tempData["label"] = parseData[data].name;
-//     tempData["color"] = "#4C0F89";
-//     colourOptions.push(tempData);
-//   }
-//   return {
-//     props: {
-//       tags: colourOptions,
-//     },
-//   };
-// };
 
 export default addproblems;
