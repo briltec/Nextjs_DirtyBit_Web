5a827a53699ce0f054aa70b682e0af256f0ccaad
briltec <palmerliu0625@gmail.com>
2022-02-01 12:53:45 +0530

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { createWrapper } from "next-redux-wrapper";
-import type { AppProps } from 'next/app'
-import { Toast } from 'primereact/toast';
-import { ToastContainer } from 'react-toastify';
+import type { AppProps } from "next/app";
+import { Toast } from "primereact/toast";
+import { ToastContainer } from "react-toastify";
 import Navbar from "../components/Navbar";
 // import Footer from "../components/Footer";
 import { store } from "../redux/store";
@@ -11,42 +11,42 @@ import "../styles/Editor.css";
 import "../styles/tinymce.css";
 import { NextPage } from "next";
 import { ReactNode } from "react";
-import 'react-toastify/dist/ReactToastify.css';
-import { createTheme, NextUIProvider } from "@nextui-org/react"
+import "react-toastify/dist/ReactToastify.css";
+import { createTheme, NextUIProvider, Text } from "@nextui-org/react";
 
 type Page<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactNode) => ReactNode;
-}
+};
 
 type Props = AppProps & {
   Component: Page;
-}
+};
 
 const theme = createTheme({
-  type:'dark',
+  type: "dark",
   theme: {
     colors: {
       // brand colors
-      primaryLight: '#6366F1',
-      primary: '#6366F1',
-      primaryDark: '#6366F1',
-      secondary: '#fff',
+      primaryLight: "#6366F1",
+      primary: "#6366F1",
+      primaryDark: "#6366F1",
+      secondary: "#fff",
 
-      background: '#1d1d1d',
-      gradient: 'linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)',
+      background: "#000000",
+      gradient:
+        "linear-gradient(112deg, $blue100 -25%, $pink500 -10%, $purple500 80%)",
       // you can also create your own color
-      myColor: '#6366F1',
+      myColor: "#6366F1",
 
       // ...  more colors
     },
     space: {},
-    fonts: {}
-  }
-})
+    fonts: {},
+  },
+});
 
 
 function MyApp({ Component, pageProps }: Props) {
-  
   if (typeof window !== "undefined") {
     import("tw-elements/dist/js/index.min.js");
   }
@@ -56,7 +56,7 @@ function MyApp({ Component, pageProps }: Props) {
   const router = useRouter();
 
   let background: string;
-  
+
   let fixedTop = "";
   if (router.pathname === "/") {
     background = "bg-transparent-800 w-screen absolute z-50";
@@ -66,20 +66,22 @@ function MyApp({ Component, pageProps }: Props) {
   }
   return (
     <>
-    <NextUIProvider theme={theme}>
-      <ToastContainer position="top-right"
-autoClose={5000}
-hideProgressBar={false}
-newestOnTop={false}
-closeOnClick
-rtl={false}
-pauseOnFocusLoss
-draggable
-theme="dark"
-pauseOnHover/>
-      <Navbar fixedHeader={fixedTop} bg={background} />
-      <Component {...pageProps} />
-    </NextUIProvider>
+      <NextUIProvider theme={theme}>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          theme="dark"
+          pauseOnHover
+        />
+        <Navbar fixedHeader={fixedTop} bg={background} />
+        <Component {...pageProps} />
+      </NextUIProvider>
     </>
   );
 }
