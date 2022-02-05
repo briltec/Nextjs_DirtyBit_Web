5780a25bd3f11997ec4af5c37579d1578ffc6e4e
briltec <palmerliu0625@gmail.com>
2022-02-05 12:21:37 +0530

    updated

--- redux/reducers/ProblemPageReducers/EditorValueReducer.tsx
+++ redux/reducers/ProblemPageReducers/EditorValueReducer.tsx
@@ -1,9 +1,17 @@
 import { UpdateEditorValue } from "../../types";
 
-let initial =
+type Action = {
+  type: string;
+  payload: string;
+};
+
+let initial: string =
   "#include<iostream>\nusing namespace std;\n\nint main(){\n\n  return 0;\n}";
 
-export const EditorValueReducer = (state = initial, action) => {
+export const EditorValueReducer = (
+  state: string = initial,
+  action: Action
+): string => {
   switch (action.type) {
     case UpdateEditorValue:
       return action.payload;
