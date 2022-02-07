a67cfacce2aac818e14526951256e5a4a4889f59
briltec <palmerliu0625@gmail.com>
2022-02-07 18:20:07 +0530

    mantine menu btn added

--- components/Navbar/Notification.tsx
+++ components/Navbar/Notification.tsx
@@ -1,28 +1,52 @@
-import { useRef } from "react";
+// import { useRef } from "react";
 import { Badge } from 'primereact/badge';
-import { Menu as PrimeMenu } from "primereact/menu";
+// import { Menu as PrimeMenu } from "primereact/menu";
 import { Button as Btn } from "primereact/button";
+// import { useSelector } from "react-redux";
 
- const BadgeDemo = () => {
-    const menu = useRef(null);
-    const items = [
-        {
-          items: [
-            {
-              label: "Some Notifications",
-            },    
-          ],
-        },
-      ];
+//  const BadgeDemo = () => {
+//     const menu = useRef(null);
+//     // @ts-ignore
+//     const isLoggedIn = useSelector(state => state.userData.is_logged_in)
+//     const items = [
+//         {
+//           items: [
+//             {
+//               label: "Some Notifications",
+//             },    
+//           ],
+//         },
+//       ];
+//     let markup: JSX.Element;
+//     if(isLoggedIn){
+//       markup = (
+//         <>
+//         <PrimeMenu className="" model={items} popup ref={menu} id="popup_menu" />
+//                        <Btn icon={<i className="pi pi-bell mr-4 p-text-secondary p-overlay-badge" style={{ fontSize: '1.3rem' }}><Badge value="1" ></Badge></i>} onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup />             
+                       
+//                </>
+//       )
+//     }else {
+//       markup = (
+//         <>
+//         </>
+//       )
+//     }
+      
+//     return markup;
+// }
 
-    return (
-        <div>
- <PrimeMenu className="" model={items} popup ref={menu} id="popup_menu" />
-                <Btn icon={<i className="pi pi-bell mr-4 p-text-secondary p-overlay-badge" style={{ fontSize: '1.3rem' }}><Badge value="1" ></Badge></i>} onClick={(event) => menu.current.toggle(event)} aria-controls="popup_menu" aria-haspopup />             
-                
-        </div>
-    );
+// export default BadgeDemo
+ 
+import { Menu, Divider } from '@mantine/core';
+
+function Notification() {
+  return (
+    <Menu control={<Btn icon={<i className="pi pi-bell mr-4 p-text-secondary p-overlay-badge" style={{ fontSize: '1.3rem' }}><Badge value="1" ></Badge></i>} aria-haspopup />   }>
+      <Menu.Item>Welcome to DirtyBits</Menu.Item>
+      <Divider/>
+    </Menu>
+  );
 }
 
-export default BadgeDemo
- 
+export default Notification;
