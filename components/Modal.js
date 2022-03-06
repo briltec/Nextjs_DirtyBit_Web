9e6d4cbe92e7c70b8186ad1a0c57a81c2155739f
briltec <palmerliu0625@gmail.com>
2022-03-06 21:12:30 +0530

    code refactored

--- components/Modal.js
+++ components/Modal.js
@@ -1,5 +1,4 @@
 import React from "react";
-import success from "../public/success.png";
 import Image from "next/image";
 import Link from "next/link";
 
--- pages/problemset.tsx
+++ pages/problemset.tsx
@@ -1,16 +1,13 @@
-import { ReactElement, useEffect, useContext } from "react";
+import { ReactElement, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Head from "next/head";
 
 import Problem from "../components/Problem";
-import { getProblems, getTags } from "../redux/actions";
-import {Context} from '../Context'
+import { getProblems } from "../redux/actions";
 
 function Practice(): ReactElement {
   const dispatch = useDispatch();
-  const {tags} = useContext(Context)
 
-  console.log('state', tags)
   useEffect(() => {
     dispatch(getProblems());
   }, []);
