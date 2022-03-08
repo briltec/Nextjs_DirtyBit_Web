1209ed80525d4bbcc217ebba16576ed13b8de45b
briltec <palmerliu0625@gmail.com>
2022-03-08 22:26:38 +0530

    code refactored

--- components/Modal.js
+++ components/Modal.js
@@ -9,7 +9,6 @@ function Modal({ title, content, source }) {
         className="min-w-screen h-screen animated fadeIn faster  fixed  left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none bg-no-repeat bg-center bg-cover"
         id="modal-id"
       >
-        {/* <div className="absolute bg-black opacity-80 inset-0 z-0"></div> */}
         <div className="w-[20rem] md:w-full lg:w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg  bg-white ">
           <>
             <div className="text-center p-5 flex-auto justify-center">
