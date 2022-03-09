8619f29e04f6e22d8fc38aaaadb48a8a432c99c9
briltec <palmerliu0625@gmail.com>
2022-03-09 22:38:59 +0530

    debounce funtion

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+export const debounce = (fn, delay) => {
+  let timeoutID;
+  return function (...args) {
+    if (timeoutID) clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
