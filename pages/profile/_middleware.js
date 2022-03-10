09230fad42d384324e22ac135e19efa8c43ab765
briltec <palmerliu0625@gmail.com>
2022-03-10 22:35:02 +0530

    middleware code refactored

--- pages/profile/_middleware.js
+++ pages/profile/_middleware.js
@@ -1,7 +1,6 @@
 import { NextResponse, NextRequest } from "next/server";
 
 export async function middleware(req, ev) {
-  console.log("req", req.nextUrl);
   if (!req.cookies["access"]) {
     return NextResponse.redirect("/auth/signin");
   }
