6fe83379c31bef892e24d9f2e3835cd24dd89659
briltec <palmerliu0625@gmail.com>
2022-03-10 22:35:34 +0530

    prevents sign in and signup if logged in

---
+++ pages/auth/_middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const token = req.cookies["access"];
+
+  const { pathname } = req.nextUrl;
+
+  if (
+    (pathname.includes("/auth/signin") || pathname.includes("/auth/signup")) &&
+    !token
+  ) {
+    return NextResponse.next();
+  }
+
+  if (token && (pathname === "/auth/signup" || pathname === "/auth/signin")) {
+    return NextResponse.redirect("/");
+  }
+}
