28f471ea81286cad8c5c580b375db0f9a6de8a33
briltec <palmerliu0625@gmail.com>
2022-05-05 22:07:02 +0530

    title added to the index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from 'components/Hero'
 import Feature from 'components/Feature/index'
+import Head from 'next/head'
 
 function Navbar() {
   return (
     <>
+    <Head><title>DirtyBits</title></Head>
     <Hero />
     <Feature title="Features" description="Some of our basics features"/>
     </>
