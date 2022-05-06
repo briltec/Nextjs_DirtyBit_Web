3b88077742c549118f9daca0577715199591b2a0
briltec <palmerliu0625@gmail.com>
2022-05-06 21:22:59 +0530

    log removed

--- hooks/htmlParser.js
+++ hooks/htmlParser.js
@@ -2,7 +2,6 @@ import DOMPurify from "dompurify";
 import parse from "html-react-parser";
 
 function useHtmlParser(htmlString = "") {
-  console.log("htmlString", htmlString);
   const cleanHtmlString = DOMPurify.sanitize(htmlString, {
     USE_PROFILES: { html: true },
   });
