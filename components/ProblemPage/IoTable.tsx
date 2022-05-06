a7d1877adf81272357dea3c6029a8a929a006319
briltec <palmerliu0625@gmail.com>
2022-05-06 21:23:44 +0530

    border fixed

--- components/ProblemPage/IoTable.tsx
+++ components/ProblemPage/IoTable.tsx
@@ -6,31 +6,35 @@ interface Props {
 }
 
 function IoTable(props: Props): ReactElement {
+
+  const tableBorder = {
+    border: "1px solid #000",
+  }
   return (
-    <div>
-      <table className="border-collapse w-3/6">
+    <>
+      <table className="border-collapse w-3/6 mb-6">
         <thead>
           <tr>
-            <th className="p-3 font-bold uppercase bg-[#1F2937] text-white border border-white hidden lg:table-cell">
+            <th style={tableBorder} className="p-3 font-bold uppercase bg-[#1F2937] text-white hidden lg:table-cell">
               Input
             </th>
-            <th className="p-3 font-bold uppercase bg-[#1F2937] text-white border border-white hidden lg:table-cell">
+            <th style={tableBorder} className="p-3 font-bold uppercase bg-[#1F2937] text-white hidden lg:table-cell">
               Output
             </th>
           </tr>
         </thead>
-        <tbody>
+        <tbody style={tableBorder}>
           <tr className="bg-[#1F2937] flex lg:table-row flex-row lg:flex-row flex-wrap lg:flex-no-wrap mb-10 lg:mb-0">
-            <td className="w-full lg:w-auto p-3 text-white border border-white border-b block lg:table-cell relative lg:static">
+            <td style={tableBorder} className="w-full lg:w-auto p-3 text-white block lg:table-cell relative lg:static">
               <pre>{props.inputData}</pre>
             </td>
-            <td className="w-full lg:w-auto p-3 text-white border-white border border-b block lg:table-cell relative lg:static">
+            <td className="w-full lg:w-auto p-3 text-white block lg:table-cell relative lg:static">
               <pre>{props.outputData}</pre>
             </td>
           </tr>
         </tbody>
       </table>
-    </div>
+    </>
   );
 }
 
