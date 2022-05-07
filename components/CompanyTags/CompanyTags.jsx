76d31273699a52d007de4fe89acf89f89ddb3826
briltec <palmerliu0625@gmail.com>
2022-05-07 21:25:41 +0530

    underline fixed

--- components/CompanyTags/CompanyTags.jsx
+++ components/CompanyTags/CompanyTags.jsx
@@ -5,13 +5,12 @@ import Image from 'next/image'
 function CompanyTags({title, Icon}) {
   return (
     <div className="bg-slate-500 py-3 opacity-70 flex flex-col items-center rounded-md justify-center">
-      {/* <div className="flex justify-center items-center h-full w-full rounded-md  bg-white"> */}
       <div>
         {Icon}
       </div>
       <a
         href="#"
-        className="text-white text-center text-sm  font-lato"
+        className="text-white text-center text-sm font-lato no-underline"
         >
           {title}
       </a>
