a20665e861a3215a899cbb5044ae754c60e7e481
briltec <palmerliu0625@gmail.com>
2022-05-09 21:09:08 +0530

    code refactored

---
+++ components/Dropdown/index.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Button, Menu} from "@mantine/core";
+import {
+  ChevronDown,
+} from "tabler-icons-react";
+
+export default function ButtonMenu({ label, list, handleClick}) {
+  return (
+    <Menu
+      control={
+        <Button
+          rightIcon={<ChevronDown size={18} />}
+          sx={{ paddingRight: 12 }}
+          variant="white"
+          className="text-black font-semibold"
+          radius={30}
+        >
+          {label}
+        </Button>
+      }
+      transition="pop-top-right"
+      placement="end"
+      size="lg"
+    >
+      <ul className="max-h-60 overflow-scroll scrollbar-hide">
+        {list.map((item: JSX.Element) => {
+          if (item.key !== null) {
+            return (
+                  <Menu.Item
+                    onClick={() => handleClick(item)}
+                  >
+                    {item.props.children}
+                  </Menu.Item>
+            );
+          }
+        })}
+      </ul>
+    </Menu>
+  );
+}
