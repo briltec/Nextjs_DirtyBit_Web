c9dc09623f71fa2d8b23ee1cf5d83068b8831410
briltec <palmerliu0625@gmail.com>
2022-05-12 21:34:15 +0530

    Terminal and Navbar added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,23 @@ import React from 'react'
 import Hero from 'components/Hero'
 import Feature from 'components/Feature/index'
 import Head from 'next/head'
+import Navbar from 'components/NavbarT'
+import Terminal from 'components/Terminal'
 
-function Navbar() {
+function Homepage() {
   return (
-    <>
-    <Head><title>DirtyBits</title></Head>
-    <Hero />
-    <Feature title="Features" description="Some of our basics features"/>
-    </>
+    <div className='backgroundGrdient'>
+      <Navbar/>
+      <Head><title>DirtyBits</title></Head>
+      <Hero />
+      <Terminal/>
+      <Feature title="Features" description="Some of our basics features"/>
+    </div>
   )
 }
 
-export default Navbar
+export default Homepage;
+
+Homepage.getNavbarTLayout = function PageLayout(page: any) {
+  return <>{page}</>;
+};
