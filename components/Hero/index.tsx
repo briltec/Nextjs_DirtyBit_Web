83bfd28b1ced1497478e7d49478de33970fca2b4
briltec <palmerliu0625@gmail.com>
2022-05-12 15:41:01 +0530

    Color Theme added

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   createStyles,
   Container,
@@ -7,63 +7,63 @@ import {
   Group,
   Text,
   Image,
-} from '@mantine/core';
-import hero from "public/hero.svg"
-import { colors } from 'constants/colors';
-import Link from 'next/link';
+} from "@mantine/core";
+import hero from "public/hero.svg";
+import { colors } from "constants/colors";
+import Link from "next/link";
 
 const useStyles = createStyles((theme) => ({
   inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    display: "flex",
+    justifyContent: "space-between",
     paddingTop: theme.spacing.xl * 4,
     paddingBottom: theme.spacing.xl * 4,
-    alignItems: 'center',
+    alignItems: "center",
   },
 
   content: {
     maxWidth: 500,
     marginRight: theme.spacing.xl * 3,
 
-    [theme.fn.smallerThan('md')]: {
-      maxWidth: '100%',
+    [theme.fn.smallerThan("md")]: {
+      maxWidth: "100%",
       marginRight: 0,
     },
   },
 
   title: {
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
     fontFamily: `Greycliff CF, ${theme.fontFamily}`,
     fontSize: 44,
     lineHeight: 1.2,
     fontWeight: 900,
 
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan("xs")]: {
       fontSize: 28,
     },
   },
 
   control: {
-    [theme.fn.smallerThan('xs')]: {
+    [theme.fn.smallerThan("xs")]: {
       flex: 1,
     },
   },
 
   image: {
-    display: 'none',
-    [theme.fn.smallerThan('md')]: {
-      display: 'none',
+    display: "none",
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
     },
   },
 
   highlight: {
-    position: 'relative',
+    position: "relative",
     backgroundColor:
-      theme.colorScheme === 'dark'
+      theme.colorScheme === "dark"
         ? theme.fn.rgba(colors.primary, 0.55)
         : theme.colors[theme.primaryColor][0],
     borderRadius: theme.radius.sm,
-    padding: '4px 12px',
+    padding: "4px 12px",
   },
 }));
 
@@ -77,24 +77,39 @@ export default function HeroBullets() {
             <Title className={classes.title}>
               Welcome to <span className={classes.highlight}>DirtyBits</span>
             </Title>
-            <Text color="#fff" size="xl" mt="md">
-            Be a Coder with us.
+            {/* <Text color="#fff" size="xl" mt="md">
+             */}
+            <Text color="customColor" size="xl" mt="md">
+              Be a Coder with us.
             </Text>
-            <Text color="dimmed" mt="md">First, solve the problem. Then, write the code.</Text>
+            <Text color="dimmed" mt="md">
+              First, solve the problem. Then, write the code.
+            </Text>
 
             <Group mt={30}>
               <Link href="/problemset" passHref>
-                <Button variant='gradient' gradient={{ from: colors.primary, to: '#835FE6', deg: 35 }} radius="xl" size="lg" className={classes.control}>
+                <Button
+                  variant="gradient"
+                  gradient={{ from: colors.primary, to: "#835FE6", deg: 35 }}
+                  radius="xl"
+                  size="lg"
+                  className={classes.control}
+                >
                   Explore
                 </Button>
               </Link>
             </Group>
           </div>
-          <Image src={hero.src} width={500} height={500} alt="hero right image" sx={{
-            '@media (max-width: 1200px)': {
-              display: 'none',
-            },
-            }} 
+          <Image
+            src={hero.src}
+            width={500}
+            height={500}
+            alt="hero right image"
+            sx={{
+              "@media (max-width: 1200px)": {
+                display: "none",
+              },
+            }}
           />
         </div>
       </Container>
--- theme/theme.js
+++ theme/theme.js
@@ -1,3 +1,18 @@
 module.exports = {
   colorScheme: "dark",
+  colors: {
+    customColor: [
+      "#EFEBFA",
+      "#D3C6F0",
+      "#B7A2E7",
+      "#9B7DDD",
+      "#7F59D4",
+      "#6334CB",
+      "#4F2AA2",
+      "#3B1F7A",
+      "#281551",
+      "#140A29",
+    ],
+  },
+  primaryColor: "customColor",
 };
