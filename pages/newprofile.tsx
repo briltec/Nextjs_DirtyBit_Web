9781d287304d34f2fc3cdf5f768023dbd8cb799a
briltec <palmerliu0625@gmail.com>
2022-05-15 21:47:22 +0530

    New profile page

---
+++ pages/newprofile.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import SideNavbar from 'components/SideNavbar'
+import ProfileNavbar from 'components/ProfileNavbar'
+import { Container, Grid, Group, Skeleton } from '@mantine/core';
+
+import LeftSection from 'components/Profile/LeftSection'
+import RightSection from 'components/Profile/RightSection'
+
+function Profile() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    // const child = <Skeleton height={140} radius="md" animate={true} />;
+  return (
+      <div className='relative flex profilePageBackground'>
+          <div className='absolute top-52 left-36 profilePagePatter1'></div>
+          <div className='absolute top-72 right-[25%] profilePagePatter2'></div>
+          {isOpen && <SideNavbar />}
+          <div className='hidden md:block'>
+            <SideNavbar/>
+          </div>
+          <div className='flex-1 p-5 lg:p-10'>
+            <ProfileNavbar setModalState={setIsOpen}/>
+            <Grid>
+                <Grid.Col xs={isOpen ? 12 : 10} className="p-4">
+                    <LeftSection/>
+                </Grid.Col>
+                <Grid.Col xs={2} className="hidden lg:block p-4 overflow-hidden">
+                    <RightSection/>
+                </Grid.Col>
+            </Grid>
+          </div>
+      </div>
+  )
+}
+
+export default Profile
