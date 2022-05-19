879bb4dbf1af93d59047977125507a20d4ce802c
briltec <palmerliu0625@gmail.com>
2022-05-19 21:53:24 +0530

    Code refactored

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,31 @@
 import "@fontsource/space-grotesk"
 import { createWrapper } from "next-redux-wrapper";
 import type { AppProps } from "next/app";
-import Footer from "components/Footer";
 import { store } from "redux/store";
-import "../styles/index.css";
+import { ReactNode } from "react";
 import { useRouter } from "next/router";
-import "../styles/Editor.css";
-import "../styles/tinymce.css";
 import { NextPage } from "next";
-import { ReactNode, useEffect } from "react";
-import "react-toastify/dist/ReactToastify.css";
-import theme from "theme/theme";
+import { MantineProvider } from "@mantine/core";
 import NextNProgress from "nextjs-progressbar";
 import { NotificationsProvider } from "@mantine/notifications";
-import { MantineProvider } from "@mantine/core";
+
 import {colors} from 'constants/colors';
+import Context from '../Context'
+import theme from "theme/theme";
+import Navbar from "components/Navbar2.0";
 
 import "react-quill/dist/quill.snow.css";
 import "primereact/resources/themes/lara-light-indigo/theme.css"; //theme
 import "primereact/resources/primereact.min.css"; //core css
 import "primeicons/primeicons.css";
+import "react-toastify/dist/ReactToastify.css";
 import 'styles/Profile/profile.css'
-
-
-import Context from '../Context'
-
+import "../styles/tinymce.css";
+import "../styles/Editor.css";
+import "../styles/index.css";
 
 type Page<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactNode) => ReactNode;
-  getNavbarTLayout?: (page: ReactNode) => ReactNode;
 };
 
 type Props = AppProps & {
@@ -36,10 +33,6 @@ type Props = AppProps & {
 };
 
 function MyApp({ Component, pageProps }: Props) {
-  // const dispatch = useDispatch();
-  // useEffect(() => {
-  //   dispatch(notifyFirstLoad());
-  // }, []);
   const router = useRouter();
 
   if(typeof window !== "undefined" && router.pathname === '/'){
@@ -52,32 +45,13 @@ function MyApp({ Component, pageProps }: Props) {
       }
     })
   }
-  // let footer: JSX.Element;
 
-  // if (router.pathname === "/") {
-  //   footer = <Footer />;
-  // } else {
-  //   footer = null;
-  // }
-  
-  
   if (Component.getLayout) {
     return Component.getLayout((
       <MantineProvider theme={theme}>
         <NotificationsProvider position="top-right" zIndex={2077}>
-        <Component {...pageProps} />
-        </NotificationsProvider>
-      </MantineProvider>
-    ));
-  }
-
-  // FOR HOME SCREEN
-  if(Component.getNavbarTLayout){
-    return Component.getNavbarTLayout((
-      <MantineProvider theme={theme}>
-        <NotificationsProvider position="top-right" zIndex={2077}>
-        <Component {...pageProps} />
-        {/* {footer} */}
+          <NextNProgress height={2} color={colors.primary} />
+          <Component {...pageProps} />
         </NotificationsProvider>
       </MantineProvider>
     ));
@@ -87,11 +61,10 @@ function MyApp({ Component, pageProps }: Props) {
     <>
     <Context>
       <MantineProvider theme={theme}>
-        <NotificationsProvider position="top-right" zIndex={2077} color="red">
+        <NotificationsProvider position="top-right" zIndex={2077}>
           <NextNProgress height={2} color={colors.primary} />
-          {/* <Navbar /> */}
+          <Navbar />
           <Component {...pageProps} />
-          {/* {footer} */}
         </NotificationsProvider>
       </MantineProvider>
     </Context>
