7d59476c963addc934cd007828b718a62af70110
briltec <palmerliu0625@gmail.com>
2022-05-21 21:50:14 +0530

    Code refactored

--- components/Problem/index.tsx
+++ components/Problem/index.tsx
@@ -2,8 +2,8 @@ import { ReactElement, useCallback, useContext, useEffect, useState, } from "rea
 
 import { AiOutlineSearch } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { useSelector } from "react-redux";
-import { MultiSelect , Input } from '@mantine/core';
+import { useDispatch, useSelector } from "react-redux";
+import { MultiSelect , Input, Divider } from '@mantine/core';
 import { CheckboxGroup, Checkbox } from '@mantine/core';
 
 import Table from "../Table";
@@ -15,53 +15,54 @@ import { problemListI } from "../../redux/interfaces";
 import { Context } from "../../Context";
 import {debounce} from '../../utils'
 import Fade from 'react-reveal/Fade';
+import { getProblems, updateProblemList } from "redux/actions";
 
-
-function Problem(props): ReactElement {
-  const companyData = [
-    {
-      id: 1,
-    Icon: <GoogleIcon/>,
-    name: "Google Questions",
-  }, 
+const companyData = [
   {
-    id: 2,
-    Icon: <FacebookIcon/>,
-    name: "Facebook Questions",
-  },
-  {
-    id: 3,
-    Icon: <AmazonIcon/>,
-    name: "Amazon Questions",
-  },
-  {
-    id: 4,
-    Icon: <MicrosoftIcon/>,
-    name: "Microsoft Questions",
-  },
-  {
-    id: 5,
-    Icon: <PlusIcon/>,
-    name: "Plus",
-  }
+    id: 1,
+  Icon: <GoogleIcon/>,
+  name: "Google Questions",
+}, 
+{
+  id: 2,
+  Icon: <FacebookIcon/>,
+  name: "Facebook Questions",
+},
+{
+  id: 3,
+  Icon: <AmazonIcon/>,
+  name: "Amazon Questions",
+},
+{
+  id: 4,
+  Icon: <MicrosoftIcon/>,
+  name: "Microsoft Questions",
+},
+{
+  id: 5,
+  Icon: <PlusIcon/>,
+  name: "Plus",
+}
 ]
 
+function Problem(props): ReactElement {
+  const dispatch = useDispatch()
+  
   const [searchQuery, setSearchQuery] = useState<string>("");
   const [difficulty, setDifficulty] = useState<string[]>([]);
   const [tags, setTags] = useState([]);
-  const [currentDataList, setCurrentDataList] = useState<problemListI[]>([]);
-  
+  // const [currentDataList, setCurrentDataList] = useState<problemListI[]>([]);
   const {tags :tagsList} = useContext(Context)
   const problemList = useSelector((state: any) => state.problemList);
   
   useEffect(() => {
     function getData() {
       if(tags.length === 0 && difficulty.length === 0 && searchQuery.length === 0){
-        setCurrentDataList(problemList);
+        dispatch(getProblems())
       }
     }
     getData();
-  }, [problemList, tags, searchQuery, difficulty]);
+  }, [tags, searchQuery, difficulty, dispatch]);
 
   const fetchFilteredData = async (searchQuery = '') => {
     const {data} = await filterProblemData.post<problemListI[]>("/", {
@@ -69,7 +70,7 @@ function Problem(props): ReactElement {
       tags: tags,
       difficulty: difficulty,
     });
-    setCurrentDataList(data)
+    dispatch(updateProblemList(data))
   }
   
   useEffect(() => {
@@ -94,8 +95,7 @@ function Problem(props): ReactElement {
             </Fade>
           ))}
         </div>
-      <hr className="!mb-10 bg-custom-indigo"/>
-
+      <Divider/>     
       <MultiSelect
       // @ts-ignore
         data={tagsList}
@@ -129,7 +129,7 @@ function Problem(props): ReactElement {
           <Checkbox value="H" label="Hard" />
         </CheckboxGroup>
         <div className="flex flex-col">
-        <Table dataList={currentDataList} />
+        <Table dataList={problemList} />
         </div>
       </div>
     </WrapperLayout>
--- components/Profile/LeftSection/index.js
+++ components/Profile/LeftSection/index.js
@@ -4,12 +4,11 @@ import ActivityFeed from "components/Profile/ActivityFeed";
 
 function ProfileLeftSection() {
   return (
-    <div className="glassBackground p-10 rounded-xl flex flex-col justify-between">
+    <div className="glassBackground p-10 rounded-xl flex flex-col justify-between gap-10">
       <div className="flex justify-between items-center">
         <UserCard />
         <ActivityFeed />
       </div>
-      <UserCard />
       <UserCard />
     </div>
   );
