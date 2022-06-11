b3ce9ee8ccf2e8871c379dd60aaeb29373d52bd7
briltec <palmerliu0625@gmail.com>
2022-06-11 21:52:59 +0530

    memoized componen

--- components/CompanyTags/CompanyTags.jsx
+++ components/CompanyTags/CompanyTags.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import Fade from 'react-reveal/Fade';
+
 function CompanyTags({title, Icon}) {
   return (
+  <Fade bottom>
     <div className="bg-slate-500 py-3 opacity-70 flex flex-col items-center rounded-md justify-center">
-      <div>
-        {Icon}
-      </div>
-      <a
-        href="#"
-        className="text-white text-center text-sm font-lato no-underline"
-        >
-          {title}
-      </a>
-  </div>
+        <div>
+          {Icon}
+        </div>
+        <a
+          href="#"
+          className="text-white text-center text-sm font-lato no-underline"
+          >
+            {title}
+        </a>
+    </div>
+  </Fade>
   )
 }
 
